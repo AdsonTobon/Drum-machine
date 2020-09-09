81f1c9a88a7d903db20b708a0ef59489edf08b17
Anderson Tobon <anderson.tobonp@gmail.com>
2020-09-08 22:58:58 -0500

    Creando componentes y archivos de audios

---
+++ src/components/Drum.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Audios from '../services/Audios';
+
+export default function Drum() {
+
+const sound = Audios;
+
+var bleep =new Audio()
+
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
